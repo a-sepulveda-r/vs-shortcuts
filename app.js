75470f042491247c4ec37f47cfd1333187cdbe75
a-sepulveda-r <ariel.sepulvedar@gmail.com>
2022-11-13 16:31:19 -0300

    commit1

---
+++ app.js
@@ -0,0 +1,17 @@
+// ctrl + s guardar cambios
+
+// windows + .  para hacer emojis
+
+console.log("manolito 34: quiero una pizza we 😎");
+console.error("pizzero: NO HAY WE!! 😣🤬🤬");
+console.log("manolito 34: pero busca we: 🥴");
+console.warn("Ya voy a buscar we... 🙄🙄");
+console.warn("manolito 34: pero busca we: 🥴");
+console.log("ya toma we 😑: 🍕🍕🍕");
+// ccl→	console.clear()
+// cer→	console.error(object)
+// cgr→	console.group(label)
+// cge→	console.groupEnd()
+// ctr→	console.trace(object)
+// cwa→	console.warn
+// cin→	console.info
